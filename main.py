3e3ae6c977cb3434349f70b28ba87e1373abfa4b
skleenn <113645968+skleenn@users.noreply.github.com>
2024-02-10 13:29:55 -0800

    i am wiating for pinterest wahwah

---
+++ main.py
@@ -0,0 +1,21 @@
+import discord
+from discord import app_commands
+#import interaction
+
+
+#bot = interactions.Client(token = "MTIwNTY3ODc1MDMxNDc5NTAyOQ.Gu7QgF.BuXr1309EkZV6M6Pmx0Wo7cq1Irz3gnwq8zvSc")
+intents = discord.Intents.default()
+intents.message_content = True
+activity=discord.Activity(type=discord.ActivityType.watching, name="your pinterest >:(")
+client = discord.Client(command_prefix='/', activity=activity, intents=intents)
+tree = app_commands.CommandTree(client)
+
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+
+client.run('MTIwNTY3ODc1MDMxNDc5NTAyOQ.Gu7QgF.BuXr1309EkZV6M6Pmx0Wo7cq1Irz3gnwq8zvSc')
+
+
+
